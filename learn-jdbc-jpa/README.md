5298380575b521b061e21a4e0473e26832a49c2b
Yerlan Nurlibayev <106547481+Aiknn@users.noreply.github.com>
2025-11-14 12:41:49 +0000

    added new jdbc project

---
+++ learn-jdbc-jpa/README.md
@@ -0,0 +1,2 @@
+* to run project - mvn spring-boot:run
+* add to properties - spring.h2.console.enabled=true
---
+++ learn-jdbc-jpa/src/test/java/com/yerlan/learn_jdbc_jpa/LearnJdbcJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yerlan.learn_jdbc_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJdbcJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
